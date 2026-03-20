bb73d14e39d91f4f418e2cbfd5f9c7551697b05c
Técio Veneza <tecio.veneza@gmail.com>
2026-03-19 21:10:26 -0300

    Teste plano

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -7,6 +7,7 @@ import LoginPageClient from "./LoginPageClient"
 export default async function LoginPage({ searchParams }: LoginPageProps) {
   const params = await searchParams
   const initialPlan = params?.plan || "profissional"
+  console.log("Plano vindo da URL:", initialPlan)
 
   return <LoginPageClient initialPlan={initialPlan} />
 }
